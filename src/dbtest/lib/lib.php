42ccacc25206577bfdfcdfef3e73919f15f3da69
mritchi <mritchi@yahoo.com>
2017-01-24 19:09:33 -0500

    Update and rename src/lib/lib.php to src/dbtest/lib/lib.php

---
+++ src/dbtest/lib/lib.php
@@ -0,0 +1,18 @@
+<?php
+
+
+/*
+ * Trait for get ancestors method for marzlib
+ * 
+ * @author Matt Ritchie <mritchi at yahoo dot com>
+ * @copyright 2017 
+ * @license http://opensource.org/licenses/BSD-3-ClauseNew BSD License
+ * @package marzlib
+ * @category PHP
+ * @subpackage filelib
+ * @version 
+ * @todo
+ 
+ */
+namespace dbtest\lib;
+?>
